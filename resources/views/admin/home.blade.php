e2760cdba59de76650932738d4d1a95718c829cd
Martijn Buurman <martijn@m19.nl>
2015-05-17 13:28:58 +0200

    Add admin page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,11 @@
-hello
+@extends('app')
+
+@section('content')
+
+  <div class="container">
+    <p>
+      Beheerders pagina voor UWV Quickscan.
+    </p>
+  </div>
+
+@endsection
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>UWV Quickscan</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link href="{{ asset('/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -20,7 +17,7 @@
 </head>
 <body>
 	<nav class="navbar navbar-default">
-		<div class="container-fluid">
+		<div class="container">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle Navigation</span>
@@ -28,12 +25,13 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#">UWV</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/') }}">Home</a></li>
+					<li><a href="{{ url('/vragen') }}">Vragen</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
@@ -56,7 +54,10 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+	<script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/3rdparty/blockly/blockly_compressed.js') }}"></script>
+	<script src="{{ asset('/3rdparty/blockly/blocks_compressed.js') }}"></script>
+	<script src="{{ asset('/3rdparty/blockly/msg/js/nl.js') }}"></script>
 </body>
 </html>
