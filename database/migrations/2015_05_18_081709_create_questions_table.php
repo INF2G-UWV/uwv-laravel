cc6f3e13d6a35cd121531eec2a32033761bc38c6
Martijn Buurman <martijn@m19.nl>
2015-05-18 10:38:38 +0200

    Questions migration

---
+++ database/migrations/2015_05_18_081709_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('questions', function($table)
+		{
+		    $table->increments('id');
+				$table->integer('parent_id');
+				$table->string('question', 100);
+				$table->text('block');
+				$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('questions');
+	}
+
+}
